4d3a14f74bfefbd5539604d96f120cec7faed5cd
Joshua Sherman <josh@gravityblvd.com>
2014-01-13 23:32:12 -0500

    Enabling more modules
    I don't see the need in selectively including syntax for modules when I can
    just install the modules and include them all. Expanded to include all of the
    modules I could that were in the list already and updated. Going to look into
    getting a more up to date module list for PHP 5.5+ and expanding further.

--- update_syntax.php
+++ update_syntax.php
@@ -1,123 +1,129 @@
 <?php
+
 /**
- * Script to gather up all native functions, classes, and interfaces from any release of
- * PHP for the purposes of updating the VIM syntax file.
+ * Script to gather up all native functions, classes, and interfaces from any
+ * release of PHP for the purposes of updating the VIM syntax file.
  *
  * @author Paul Garvin <paul@paulgarvin.net>
  * @copyright Copyright 2009 Paul Garvin
  * @license http://www.opensource.org/licenses/mit-license.php MIT License
  *
  * @author Stan Angeloff <stanimir@angeloff.name>
+ * @author Joshua Sherman <josh@gravityblvd.com>
  */
 
 /**
  * This script works by loading up PHP extensions and using reflection to pull
- * the functions, classes, and constants out of those extesions. The list of extensions
- * below are ones included with PHP 5.3 source code. The ones commented out depend on
- * an external library being installed, are Unix specific, or just not commonly used.
+ * the functions, classes, and constants out of those extesions. The list of
+ * extensions below are ones included with PHP 5.3 source code. The ones
+ * commented out depend on an external library being installed, are Unix
+ * specific, or just not commonly used.
  *
  * Add, comment, or uncomment to fit your needs or particular PHP installation.
- * Remember that some of these extensions are likely shared extensions and must be
- * enabled in your php.ini file.
+ * Remember that some of these extensions are likely shared extensions and must
+ * be enabled in your php.ini file.
  *
- * NOTE: mysqlnd is not included because it exposes no functions, classes, or constants.
- * The pdo_* extensions are not included in the list because they do not expose any
- * functions, classes, or constants themselves. The constants and methods specific
- * to that driver are exposed though the PDO extension itself. The pdo_* extensions
- * must still be enabled (compiled in or loaded as shared) for these constants to show up.
+ * NOTE: mysqlnd is not included because it exposes no functions, classes, or
+ * constants. The pdo_* extensions are not included in the list because they do
+ * not expose any functions, classes, or constants themselves. The constants
+ * and methods specific to that driver are exposed though the PDO extension
+ * itself. The pdo_* extensions must still be enabled (compiled in or loaded as
+ * shared) for these constants to show up.
  */
-$allowed_extensions = array(
-	# 'calendar',
-	# 'com_dotnet',
-	# 'ctype',
-	# 'dba',
-	# 'enchant',
-	# 'exif',
-	# 'fileinfo',
-	# 'filter',
-	# 'ftp',
-	# 'gmp',
-	# 'imap',
-	# 'interbase',
-	# 'intl',
-	# 'ldap',
-	# 'mssql',
-	# 'oci8',
-	# 'oci8_11g',
-	# 'odbc',
-	# 'pcntl',
-	# 'posix',
-	# 'pspell',
-	# 'readline',
-	# 'recode',
-	# 'shmop',
-	# 'snmp',
-	# 'sqlite',
-	# 'sybase_ct',
-	# 'sysvmsg',
-	# 'sysvsem',
-	# 'sysvshm',
-	# 'tidy',
-	# 'xmlrpc',
-	# 'xsl',
+$allowed_extensions = [
 	'bcmath',
 	'bz2',
+	'calendar',
+	// 'com_dotnet',
 	'core',
+	'ctype',
 	'curl',
 	'date',
+	'dba',
 	'dom',
+	// 'enchant',
 	'ereg',
+	'exif',
+	'fileinfo',
+	'filter',
+	'ftp',
 	'gd',
 	'gettext',
+	// 'gmp',
 	'hash',
 	'iconv',
+	// 'imap',
+	// 'interbase',
+	'intl',
 	'json',
+	'ldap',
 	'libxml',
 	'mbstring',
 	'mcrypt',
 	'memcache',
 	'memcached',
 	'mhash',
+	// 'mssql',
 	'mysql',
 	'mysqli',
+	// 'oci8',
+	// 'oci8_11g',
+	'odbc',
 	'openssl',
 	'pcre',
 	'pdo',
 	'pgsql',
 	'phar',
+	'pcntl',
+	'posix',
+	'pspell',
+	'readline',
+	// 'recode',
 	'redis',
 	'reflection',
 	'session',
+	'shmop',
 	'simplexml',
+	'snmp',
 	'soap',
 	'sockets',
 	'spl',
+	// 'sqlite',
 	'sqlite3',
 	'standard',
+	// 'sybase_ct',
+	'sysvmsg',
+	'sysvsem',
+	'sysvshm',
+	// 'tidy',
 	'tokenizer',
 	'test_helpers',
 	'wddx',
 	'xdebug',
 	'xml',
 	'xmlreader',
+	'xmlrpc',
 	'xmlwriter',
+	'xsl',
 	'zip',
 	'zlib',
-);
+];
 
-$processed = array();
+$processed = [];
 
-foreach ($allowed_extensions as $extension) {
-
-	try {
-		$details = array();
+foreach ($allowed_extensions as $extension)
+{
+	try
+	{
+		$details = [];
 		$options = new ReflectionExtension($extension);
 
-		$classes   = array();
+		$classes   = [];
 		$functions = array_keys($options->getFunctions());
 		$constants = array_keys($options->getConstants());
 
-		foreach ($options->getClasses() as $class) {
+		foreach ($options->getClasses() as $class)
+		{
 			$classes[] = $class->getName();
 			$constants = array_merge($constants, array_keys($class->getConstants()));
 		}
@@ -126,26 +132,35 @@ foreach ($allowed_extensions as $extension) {
 
 		$details['name'] = $options->getName();
 
-		if (sizeof ($functions)) {
+		if (sizeof ($functions))
+		{
 			$details['functions'] = implode(' ', $functions);
 		}
-		if (sizeof ($constants)) {
+
+		if (sizeof ($constants))
+		{
 			$details['constants'] = implode(' ', $constants);
 		}
-		if (sizeof ($classes)) {
+
+		if (sizeof ($classes))
+		{
 			$details['classes'] = implode(' ', $classes);
 		}
 
 		$processed[$extension] = $details;
-	} catch (Exception $e) {
+	}
+	catch (Exception $e)
+	{
 		print "ERROR: '{$extension}' -- " . $e->getMessage() . "\n";
 	}
 }
 
 $code = "syn case match\n\n";
 
-foreach ($processed as $extension) {
-	if (isset ($extension['constants'])) {
+foreach ($processed as $extension)
+{
+	if (isset ($extension['constants']))
+	{
 		$code = $code . '" ' . $extension['name'] . "\n";
 		$code = $code . 'syn keyword phpConstants ' . $extension['constants'] . " contained\n\n";
 	}
@@ -153,12 +168,17 @@ foreach ($processed as $extension) {
 
 $code = $code . "syn case ignore\n\n";
 
-foreach ($processed as $extension) {
+foreach ($processed as $extension)
+{
 	$code = $code . '" ' . $extension['name'] . "\n";
-	if (isset ($extension['functions'])) {
+
+	if (isset ($extension['functions']))
+	{
 		$code = $code . 'syn keyword phpFunctions ' . $extension['functions'] . " contained\n";
 	}
-	if (isset ($extension['classes'])) {
+
+	if (isset ($extension['classes']))
+	{
 		$code = $code . 'syn keyword phpClasses ' . $extension['classes'] . " contained\n\n";
 	}
 }
@@ -168,3 +188,5 @@ file_put_contents(
 	str_replace('${code}', $code, file_get_contents(__DIR__ . '/syntax/php.template')),
 	LOCK_EX
 );
+
+?>
